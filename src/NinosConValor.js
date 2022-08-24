7d350cc4f72e3105e12bc440fbb9f1217cf70f86
Daniel Acero <edanielacero@gmail.com>
2022-08-24 17:45:01 -0400

    no calculator numbers in ninosconvalor from calculator example

--- src/NinosConValor.js
+++ src/NinosConValor.js
@@ -3,8 +3,6 @@ import Button from './Button';
 import {useState, useEffect} from 'react';
 
 function NinosConValor() {
-  const [numero1, setNumero1] = useState('');
-  const [numero2, setNumero2] = useState('');
   let [kid_name, setKid_name] = useState('');
   let [kid_lastname, setKid_lastname] = useState('');
   let [kid_CI, setKid_CI] = useState('');
@@ -12,30 +10,6 @@ function NinosConValor() {
   let [resultado, setResultado] = useState('');
   let [kids, setKids] = useState([]);
   const LinkKids = 'https://ucb-tde-ninos-con-valor-api.herokuapp.com/api/kids';
-
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    setResultado(parseInt(numero1) + parseInt(numero2));
-  }
-
-  function handleFormSubmit2(event) {
-    event.preventDefault();
-    setResultado(parseInt(numero1) - parseInt(numero2));
-  }
-
-  function registerKid(event) {
-    // useEffect(() => {
-    //   fetch(LinkKids, {
-    //     method: "POST"
-    //   })
-    //     .then(res => res.json())
-    //     .then(
-    //       (result) => {
-    //         setKids(result);
-    //       }
-    //     )
-    // }, [])
-  }
 
   useEffect(() => {
     fetch(LinkKids)
@@ -48,29 +22,7 @@ function NinosConValor() {
   }, [])
 
   return (
-    <div className="Sumador">
-      <h3>Sumador</h3>
-      <form onSubmit={handleFormSubmit}>
-        <InputWithLabel
-          type="number"
-          id="numero1"
-          value={numero1}
-          onChange={event => setNumero1(event.target.value)}
-        >
-          Numero1:
-        </InputWithLabel>
-        <InputWithLabel
-          type="number"
-          id="numero2"
-          value={numero2}
-          onChange={event => setNumero2(event.target.value)}
-        >
-          Numero2:
-        </InputWithLabel>
-        <Button id="sumar-btn">Sumar</Button>
-      </form>
-      <Button id="restar-btn" onClick={handleFormSubmit2}>Restar</Button>
-      <div>La suma de los numeros es:{resultado}</div>
+    <div className="NinosConValor">
       <div>La lista de nenes es:
         <ul>
           {kids.map(kids => (
@@ -80,41 +32,6 @@ function NinosConValor() {
           ))}
         </ul>
       </div>
-      <form onSubmit={registerKid}>
-        <InputWithLabel
-          type="text"
-          id="kid_name"
-          value={kid_name}
-          onChange={event => setKid_name(event.target.value)}
-        >
-          Nombre:
-        </InputWithLabel>
-        <InputWithLabel
-          type="text"
-          id="kid_lastName"
-          value={kid_lastname}
-          onChange={event => setKid_lastname(event.target.value)}
-        >
-          Apeshido:
-        </InputWithLabel>
-        <InputWithLabel
-          type="text"
-          id="kid_CI"
-          value={kid_CI}
-          onChange={event => setKid_CI(event.target.value)}
-        >
-          CI:
-        </InputWithLabel>
-        <InputWithLabel
-          type="date"
-          id="kid_birthday"
-          value={kid_birthday}
-          onChange={event => setKid_birthday(event.target.value)}
-        >
-          Cumpleanitos:
-        </InputWithLabel>
-        <Button id="register-btn">Registrar</Button>
-      </form>
     </div>
   );
 }
